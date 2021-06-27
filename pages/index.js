054e43d572e4dc487e39278728a10b42abd3cbd9
Ramki Subramanian <ramki@attendease.com>
2021-06-27 12:59:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,11 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
+         <p className={styles.description}>
+          Get started by Sleeping
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
